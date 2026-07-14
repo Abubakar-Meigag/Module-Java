85216b65170113cbf2b974965ef3688152c30517
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2026-07-14 19:14:10 +0100

    complete exercise 2.1

---
+++ exercise2.1/Main.java
@@ -0,0 +1,53 @@
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double balanceCheck() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance = balance + amount;
+        } else {
+            System.out.println("Balance must be greater than 0");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.out.println("Withdraw amount must be greater than 0");
+        } else if (amount > balance) {
+            System.out.println("Withdraw amount must be less than or equal to balance");
+        }  else {
+            balance = balance - amount;
+        }
+    }
+
+}
+
+
+class BankService {
+    void withdraw(BankAccount account, double amount) {
+        account.withdraw(amount);
+    }
+    void deposit(BankAccount account, double amount) {
+        account.deposit(amount);
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(100);
+        BankService service = new BankService();
+
+        service.withdraw(account, 150);
+        service.deposit(account, 150);
+        service.withdraw(account, 250);
+        System.out.println("Balance: " + account.balanceCheck());
+    }
+}
