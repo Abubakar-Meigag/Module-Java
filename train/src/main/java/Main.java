9911f771ad7c5644b2de27963684315c5b664491
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2026-08-01 21:49:44 +0100

    arrays

---
+++ train/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import module.Train;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Train train = new Train();
+
+        train.getStringElement(train.arr);
+        train.getIntElement(train.arrayList);
+        train.getNewArray(train.newArray);
+        train.getOldArray(train.oldArray);
+    }
+
+
+}
---
+++ train/src/main/java/module/Train.java
@@ -0,0 +1,37 @@
+package module;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Train {
+
+   public List<String> arr = new ArrayList<>(Arrays.asList("Buenos Aires", "Córdoba", "La Plata"));
+   public ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+   public String[] newArray = {"a", "b", "c", "h"};
+   public String[] oldArray = new String[5];
+
+   public void getOldArray(String[] oldArray) {
+       System.out.println(Arrays.toString(oldArray));
+   }
+
+   public void getStringElement (List<String> arr){
+       for (String s : arr) {
+           System.out.println(s);
+       }
+   };
+
+   public void getNewArray(String[] newArray){
+       for(String s : newArray){
+           System.out.println(s);
+       }
+   };
+
+   public void getIntElement (List<Integer> arrayList){
+       for (Integer i : arrayList) {
+           System.out.println(i);
+       }
+   }
+
+}
+
